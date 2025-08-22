898db805b93e855e7e1364fd055e41e17e109730
Ivan <Zolotarev.iv@novomet.ru>
2025-08-22 10:37:28 +0500

    add nginx conf for ssl

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -102,81 +102,92 @@ class UserSeeder extends Seeder
 
         User::create([
             'id' => 13,
-            'name' => 'Переведенцев Данил Александрович',
-            'email' => 'Danil.Perevedentsev@novometgroup.com',
-            'password' => Hash::make('oi(*kj,kdmv'),
-        ]);
-
-        User::create([
-            'id' => 14,
-            'name' => 'Сеидов Авел Панах Оглы',
-            'email' => 'Avel.Seidov@novometgroup.com',
-            'password' => Hash::make('k8Jkhdfd32^'),
-        ]);
-
-        User::create([
-            'id' => 15,
-            'name' => 'Переведенцев Данил Александрович',
-            'email' => 'Perevedentsev.DA@gazprom-neft.ru',
-            'password' => Hash::make('kljdUJ&67f!'),
-        ]);
-
-        User::create([
-            'id' => 16,
-            'name' => 'Павлюк Анастасия Владимировна',
-            'email' => 'Anastasia.Pavliuk@novometgroup.com',
-            'password' => Hash::make('UjHY65Tg%T1'),
-        ]);
-
-        User::create([
-            'id' => 17,
-            'name' => 'Полагушина Юлия Андреевна',
-            'email' => 'Iulia.Polagushina@novometgroup.com',
-            'password' => Hash::make('OI9dmjdu&^h'),
-        ]);
-
-        User::create([
-            'id' => 18,
-            'name' => 'Рыжов Владимир Вячеславович',
-            'email' => 'Vladimir.Ryzhov@novometgroup.com',
-            'password' => Hash::make('oiKj87YH6tg'),
-        ]);
-
-        User::create([
-            'id' => 19,
             'name' => 'Тюленев Ростислав Эдуардович',
             'email' => 'Rostislav.Tiulenev@novometgroup.com',
             'password' => Hash::make('HYTbg7^YHhv'),
         ]);
 
         User::create([
-            'id' => 20,
+            'id' => 14,
             'name' => 'Kotlin',
             'email' => 'kotlin@kotlin.com',
             'password' => Hash::make('kotlin'),
         ]);
 
-        // ОП Новомет-Стрежевой 26.03.25
+        User::create([
+            'id' => 15,
+            'name' => 'Зеляева Елена Сергеевна',
+            'email' => 'Elena.Zeliaeva@novometgroup.com',
+            'password' => Hash::make('ddw98F*YHii2'),
+        ]);
+
+        // ОП Новомет-Ноябрьск
+        User::create([
+            'id' => 16,
+            'name' => 'Переведенцев Данил Александрович',
+            'email' => 'Danil.Perevedentsev@novometgroup.com',
+            'password' => Hash::make('oi(*kj,kdmv'),
+        ]);
+
+        User::create([
+            'id' => 17,
+            'name' => 'Сеидов Авел Панах Оглы',
+            'email' => 'Avel.Seidov@novometgroup.com',
+            'password' => Hash::make('k8Jkhdfd32^'),
+        ]);
+
+        User::create([
+            'id' => 18,
+            'name' => 'Переведенцев Данил Александрович',
+            'email' => 'Perevedentsev.DA@gazprom-neft.ru',
+            'password' => Hash::make('kljdUJ&67f!'),
+        ]);
+        // ---
+
+        // ОП Новомет-Нефтеюганск
+        User::create([
+            'id' => 19,
+            'name' => 'Павлюк Анастасия Владимировна',
+            'email' => 'Anastasia.Pavliuk@novometgroup.com',
+            'password' => Hash::make('UjHY65Tg%T1'),
+        ]);
+
+        User::create([
+            'id' => 20,
+            'name' => 'Полагушина Юлия Андреевна',
+            'email' => 'Iulia.Polagushina@novometgroup.com',
+            'password' => Hash::make('OI9dmjdu&^h'),
+        ]);
+
         User::create([
             'id' => 21,
+            'name' => 'Рыжов Владимир Вячеславович',
+            'email' => 'Vladimir.Ryzhov@novometgroup.com',
+            'password' => Hash::make('oiKj87YH6tg'),
+        ]);
+        // ---
+
+        // ОП Новомет-Стрежевой 26.03.25
+        User::create([
+            'id' => 22,
             'name' => 'Родионов Егор Алексеевич',
             'email' => 'Egor.Rodionov@novometgroup.com',
             'password' => Hash::make('o(8Kj87YHtg'),
         ]);
         User::create([
-            'id' => 22,
+            'id' => 23,
             'name' => 'Юсупова Луиза Исаевна',
             'email' => 'Luiza.Iusupova@novometgroup.com',
             'password' => Hash::make('T5d8Kj87YHt'),
         ]);
         User::create([
-            'id' => 23,
+            'id' => 24,
             'name' => 'Янчук Юрий Павлович',
             'email' => 'Iurii.Ianchuk@novometgroup.com',
             'password' => Hash::make('kk&&dj87YHd'),
         ]);
         User::create([
-            'id' => 24,
+            'id' => 25,
             'name' => 'Попова Татьяна Васильевна',
             'email' => 'Tatiana.Popova@novometgroup.com',
             'password' => Hash::make('o(898IjYHtg'),
@@ -185,13 +196,13 @@ class UserSeeder extends Seeder
 
         // ОП Новомет-Нижневартовск 28.03.25
         User::create([
-            'id' => 25,
+            'id' => 26,
             'name' => 'Перебинос Сергей Михайлович',
             'email' => 'Sergei.Perebinos@novometgroup.com',
             'password' => Hash::make('e3&&dj87Yff'),
         ]);
         User::create([
-            'id' => 26,
+            'id' => 27,
             'name' => 'Перебинос Александр Сергеевич',
             'email' => 'Aleksandr.Perebinos@novomet.ru',
             'password' => Hash::make('o(89555sHtg'),
@@ -200,31 +211,23 @@ class UserSeeder extends Seeder
 
         // ОП Новомет-Юг 28.03.25
         User::create([
-            'id' => 27,
+            'id' => 28,
             'name' => 'Суюндуков Рустам Рамильевич',
             'email' => 'Rustam.Suiundukov@novometgroup.com',
             'password' => Hash::make('23d8Kj87Y45'),
         ]);
         User::create([
-            'id' => 28,
+            'id' => 29,
             'name' => 'Щурин Сергей Сергеевич',
             'email' => 'Sergei.Shurin@novometgroup.com',
             'password' => Hash::make('ggg&dj87sss'),
         ]);
         User::create([
-            'id' => 29,
+            'id' => 30,
             'name' => 'Александров Станислав Владимирович',
             'email' => 'Stanislav.Aleksandrov@novometgroup.com',
             'password' => Hash::make('ew98IjYHtg@'),
         ]);
         // ---
-
-        // Новые пользователи
-        User::create([
-            'id' => 30,
-            'name' => 'Зеляева Елена Сергеевна',
-            'email' => 'Elena.Zeliaeva@novometgroup.com',
-            'password' => Hash::make('ddw98F*YHii2'),
-        ]);
     }
 }
